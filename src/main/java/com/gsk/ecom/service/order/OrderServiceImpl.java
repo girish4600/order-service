db745ccf925f03416608e6414aab0f2839f3469d
girish4600 <kelgane.girish@gmail.com>
2026-08-20 00:45:28 +0530

    GCP-12 added call to payment service

--- src/main/java/com/gsk/ecom/service/order/OrderServiceImpl.java
+++ src/main/java/com/gsk/ecom/service/order/OrderServiceImpl.java
@@ -48,8 +48,8 @@ public class OrderServiceImpl implements OrderService {
         log.info("purchaseRequest :: {}", orderRequest.purchaseRequests());
         var purchaseList = productService.purchaseProduct(orderRequest.purchaseRequests());
         log.info("======================== completed purchaseProduct ======================== \n PURCHASE LIST {} \n ======================== ",purchaseList);
-
-        var order = orders.put(orderId++, mapper.dtoToOrderEntity(orderRequest, orderId));
+        Order order = mapper.dtoToOrderEntity(orderRequest, orderId);
+        orders.put(orderId++, order);
         var paymentRequest = new PaymentRequest(
                 orderRequest.amount(),
                 orderRequest.paymentMethod(),
